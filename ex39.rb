fef9a658520b7ffede9b6b73405663c1e965b688
Patrick Jones <pmichaeljones@gmail.com>
2014-03-25 13:12:14 -0600

    Added first half of lesson

--- ex39.rb
+++ ex39.rb
@@ -6,3 +6,35 @@ states = {
 	'New York' => 'NY',
 	'Michigan' => 'MI'
 }
+
+#create a basic set of states and some cities in them
+cities = {
+	'CA' => 'San Francisco',
+	'MI' => 'Detroit',
+	'FL' => 'Jacksonville'
+}
+
+#add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# puts out some cities
+puts '-' * 10
+puts "NY State has: ", cities['NY']
+puts "OR State has: ", cities['OR']
+
+# puts some states
+puts '-' * 10
+puts "Michigan's abbreviation is:  ", states['Michigan']
+puts "Florida's abbreviation is:  ", states['Florida']
+
+# do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has: ", cities[states['Michigan']]
+puts "Florida has: ", cities[states['Florida']]
+
+#puts every state abbreviation
+puts '-' * 10
+for state, abbrev in states
+	puts "#{state} has the city #{abbrev}."
+end
